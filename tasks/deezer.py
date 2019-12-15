74d361cb016ce87fcfd7a482fe3268138014fe07
smikhailov <ko_0n@mail.ru>
2019-12-15 13:10:05 +0300

    Добавлен метод получения deezer.get_artists

--- tasks/deezer.py
+++ tasks/deezer.py
@@ -10,3 +10,29 @@ def get_deezer_id(artist, title):
     data = json.loads(response.text)
     print(f'GET deezer_id: {data["data"][0]["id"]}, {artist} - {title}')
     return data["data"][0]["id"]
+
+
+def get_artists(deezer_id):
+    url = f"https://api.deezer.com/track/{deezer_id}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    artists = []
+    for row in data["contributors"]:
+        artists.append(row["name"])
+
+    return ", ".join(artists)
+
+
+def get_chart_info():
+    url = "https://api.deezer.com/playlist/1116189381"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    chart = []
+    for track in data["tracks"]["data"]:
+        chart.append({})
+        chart[-1]["deezer_id"] = track["id"]
+        chart[-1]["title"] = track["title"]
+        # chart[-1]["artist"] = ", ".join([i["name"] for i in track["artists"]])
+        chart[-1]["position"] = len(chart)
+
+    return chart
